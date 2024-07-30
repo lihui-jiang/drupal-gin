56d5ab81fecfbac439676a5c0ca93301658bd39c
James Jeffery <james@rd.digital>
2024-07-30 08:42:14 -0700

    Added workflow transition form ID prefix to list of ignored form IDs.

--- src/GinContentFormHelper.php
+++ src/GinContentFormHelper.php
@@ -366,6 +366,7 @@ class GinContentFormHelper implements ContainerInjectionInterface {
       'views_exposed_form',
       'date_recur_modular_sierra_occurrences_modal',
       'date_recur_modular_sierra_modal',
+      'workflow-transition-',
     ];
 
     foreach ($form_ids_to_ignore as $form_id_to_ignore) {
