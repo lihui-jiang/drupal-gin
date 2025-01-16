8c6d72860bdd0f204f693ee2636ebef76f77e997
sourabhsisodia_ <sourabhsisodia010@gmail.com>
2025-01-16 14:33:03 +0530

    Issue #3500065: Fixed aria attributes.

--- src/GinContentFormHelper.php
+++ src/GinContentFormHelper.php
@@ -139,6 +139,8 @@ class GinContentFormHelper implements ContainerInjectionInterface {
 
       // Create gin_more_actions group.
       $toggle_more_actions = t('More actions');
+      $menu_id = 'gin-more-actions-' . uniqid();
+
       $form['gin_sticky_actions']['more_actions'] = [
         '#type' => 'container',
         '#multilingual' => TRUE,
@@ -147,12 +149,16 @@ class GinContentFormHelper implements ContainerInjectionInterface {
           'class' => ['gin-more-actions'],
         ],
         'more_actions_toggle' => [
-          '#markup' => '<a href="#toggle-more-actions" class="gin-more-actions__trigger trigger" data-gin-tooltip role="button" title="' . $toggle_more_actions . '" aria-controls="gin_more_actions"><span class="visually-hidden">' . $toggle_more_actions . '</span></a>',
+          '#markup' => '<a href="#toggle-more-actions" class="gin-more-actions__trigger trigger" data-gin-tooltip role="button" title="' . $toggle_more_actions . '" aria-controls="' . $menu_id . '"><span class="visually-hidden">' . $toggle_more_actions . '</span></a>',
           '#weight' => 1,
         ],
         'more_actions_items' => [
           '#type' => 'container',
           '#multilingual' => TRUE,
+          '#attributes' => [
+            'id' => $menu_id,
+            'class' => ['gin-more-actions__menu'],
+          ],
         ],
       ];
 
