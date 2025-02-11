3d4d74f087b9f48ce7e5bcec2215e5fe70ffb518
Scott Euser <scotteuser@gmail.com>
2025-02-11 11:54:46 +0000

    Update update hook

--- gin.post_update.php
+++ gin.post_update.php
@@ -10,24 +10,25 @@
  */
 function gin_post_update_set_enabled_user_theme_settings() {
   $config = \Drupal::configFactory()->getEditable('gin.settings');
+  $settings = [
+    'enable_darkmode',
+    'accent_color',
+    'focus_color',
+    'high_contrast_mode',
+    'classic_toolbar',
+    'sticky_action_buttons',
+    'layout_density',
+    'show_description_toggle',
+  ];
   if ($config->get('show_user_theme_settings') === TRUE) {
 
     // Maintain the status quo of all settings enabled when show user theme
     // settings is enabled on a site.
-    $config->set('enabled_user_theme_settings', [
-      'enable_darkmode',
-      'accent_color',
-      'focus_color',
-      'high_contrast_mode',
-      'classic_toolbar',
-      'sticky_action_buttons',
-      'layout_density',
-      'show_description_toggle',
-    ])->save();
+    $config->set('enabled_user_theme_settings', array_combine($settings, $settings))->save();
   }
   else {
 
     // Set the default to nothing enabled.
-    $config->set('enabled_user_theme_settings', [])->save();
+    $config->set('enabled_user_theme_settings', array_fill_keys($settings, 0))->save();
   }
 }
