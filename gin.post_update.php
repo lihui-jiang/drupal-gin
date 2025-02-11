55ae497c2f535c14ed6bdef102a621b17dd86139
Scott Euser <scotteuser@gmail.com>
2025-02-11 06:55:15 +0000

    Initial proof of concept

---
+++ gin.post_update.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Post update file for the Gin theme.
+ */
+
+/**
+ * Set default user enabled settings for existing installs.
+ */
+function gin_post_update_set_enabled_user_theme_settings() {
+  $config = \Drupal::configFactory()->getEditable('gin.settings');
+  if ($config->get('show_user_theme_settings') === TRUE) {
+
+    // Maintain the status quo of all settings enabled when show user theme
+    // settings is enabled on a site.
+    $config->set('enabled_user_theme_settings', [
+      'enable_darkmode',
+      'accent_color',
+      'focus_color',
+      'high_contrast_mode',
+      'classic_toolbar',
+      'sticky_action_buttons',
+      'layout_density',
+      'show_description_toggle',
+    ])->save();
+  }
+}
